6e2516637e67178fdf54961ea2623728f55eda53
veeringnugget <xxshaunataylor@gmail.com>
2026-03-24 12:36:46 +0000

    ran api using formatted string and returned json

--- quiz_app/quiz_api.py
+++ quiz_app/quiz_api.py
@@ -2,27 +2,42 @@
 
 """
 TODO:
-1. Build formatted string
 2. API call
 3. Check response
 """
+import requests
 
 # Initialise difficulty
 def get_difficulty():
     difficulty_dict = {1 : 'easy', 2 : 'medium', 3 : 'hard', 4 : 'any'}
     difficulty = int(input("Select difficulty (1, 2, 3 or 4):\n" \
     "1. Easy\n2. Medium\n3. Hard\n4. Any\n> "))
-    return difficulty_dict[difficulty]
+    return difficulty_dict[difficulty].lower()
 
 # Initialise question type
 def get_question_type():
-    type_dict = {1 : 'multiple choice', 2 : 'bool', 3 : 'any'}
+    type_dict = {1 : 'multiple', 2 : 'boolean', 3 : 'any'}
     type = int(input("Select type of questions (1, 2 or 3):\n" \
     "1. Multiple choice\n2. True or False\n3. Any\n> "))
-    return type_dict[type]
+    return type_dict[type].lower()
 
 # Build formatted string
 def build_api_string(BASE_URL, difficulty, question_type):
-    pass
+    # both "any"
+    if difficulty == "any" and question_type == "any":
+        return BASE_URL
+    # difficulty = "any", question_type != any
+    elif difficulty == "any" and question_type != "any":
+        return f"{BASE_URL}&type={question_type}"
+    # difficulty != any, question_type = "any"
+    elif difficulty != "any" and question_type == "any":
+        return f"{BASE_URL}&difficulty={difficulty}"
+    # both != any
+    else:
+        return f"{BASE_URL}&difficulty={difficulty}&type={question_type}"
 
-
+def run_api(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Error. Status Code {response.status_code}")
+    return response.json()
