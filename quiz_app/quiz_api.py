76d89ccd43da9ac12f1854a479d4a0e8aacd43a3
veeringnugget <xxshaunataylor@gmail.com>
2026-03-24 12:17:28 +0000

    Updated difficulty and question func to inc dict, updated todo

--- quiz_app/quiz_api.py
+++ quiz_app/quiz_api.py
@@ -1,24 +1,28 @@
 """ Contains code pertaining to handling API call and response """
+
 """
 TODO:
-1. Find out difficulty from the user
-2. Find out type of question (Bool or Multiple Choice)
-3. Build formatted string
-4. API call
-5. Check response
+1. Build formatted string
+2. API call
+3. Check response
 """
-
-# Base URL for quiz API
-BASE_URL = 'https://opentdb.com/api.php?amount=10'
 
 # Initialise difficulty
 def get_difficulty():
-    return input("Select difficulty (1, 2, 3 or 4):\n" \
-    "1. Easy\n2. Medium\n3. Hard\n4. Any\n> ")
+    difficulty_dict = {1 : 'easy', 2 : 'medium', 3 : 'hard', 4 : 'any'}
+    difficulty = int(input("Select difficulty (1, 2, 3 or 4):\n" \
+    "1. Easy\n2. Medium\n3. Hard\n4. Any\n> "))
+    return difficulty_dict[difficulty]
 
 # Initialise question type
 def get_question_type():
-    return input("Select type of questions (1, 2 or 3):\n" \
-    "1. Multiple choice\n2. True / False\n3. Any\n> ")
+    type_dict = {1 : 'multiple choice', 2 : 'bool', 3 : 'any'}
+    type = int(input("Select type of questions (1, 2 or 3):\n" \
+    "1. Multiple choice\n2. True or False\n3. Any\n> "))
+    return type_dict[type]
+
+# Build formatted string
+def build_api_string(BASE_URL, difficulty, question_type):
+    pass
 
 
